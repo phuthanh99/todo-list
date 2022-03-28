3cf52e2b48007f40a0a025d54ccffa3e9d3b238e
phuthanh99 <huynhnguyenphuthanh@gmail.com>
2022-03-28 16:03:07 +0700

    first commit

---
+++ controllers/Todo.js
@@ -0,0 +1,81 @@
+﻿const Todo = require('../models/Todo')
+
+
+
+exports.createTodo = (req, res, next) => {
+    const todo = new Todo(req.body);
+
+    todo.save((err, task) => {
+        if (err || !task) {
+            return res.status(400).json({
+                message: 'something went wrong'
+            });
+        } else {
+            res.json({ task })
+        }
+    })
+}
+
+exports.getAllTodo = (req, res, next) => {
+
+    Todo.find().sort("createAt").exec((err, todos) => {
+        if (err || !todos) {
+            return res.status(400).json({
+                message: 'something went wrong in finding on todos'
+            });
+        }
+        res.json(todos);
+    });
+}
+
+exports.getTodoId = (req, res, next, todoId) => {
+    Todo.findById(todoId).exec((err, todo) => {
+        if (err || !todo) {
+            return res.status(400).json({
+                error: '404 todo not found'
+            });
+        }
+        req.todo = todo;
+
+        next();
+    });
+
+
+}
+
+exports.getTodo = (req, res) => {
+    return res.json(req.todo);
+}
+
+exports.updateTodo = (req, res, next) => {
+    const todo = req.todo;
+
+    todo.task = req.body.task;
+    console.log(todo);
+
+    todo.save((err, task) => {
+        if (err || !task) {
+            return res.status(400).json({
+                message: 'something went wrong while updating'
+            });
+        } else {
+            res.json({ task })
+        }
+    })
+}
+exports.deleteTodo = (req, res, next) => {
+    const todo = req.todo;
+
+    todo.remove((err, task) => {
+        if (err || !task) {
+            return res.status(400).json({
+                message: 'something went wrong while delete'
+            });
+        } else {
+            res.json({
+                task_delete: task,
+                message: 'Todo deleted successfully'
+            });
+        }
+    })
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+﻿const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const port = 8000;
+
+const app = express();
+
+const todoRoutes = require('./routes/Todo');
+
+
+mongoose.Promise = global.Promise;
+
+// Connect MongoDB at default port 27017.
+mongoose.connect('mongodb://0.0.0.0:27017/Todo_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', todoRoutes);
+
+app.listen(port, () => {
+    console.log(`listening on port htstp://localhost:${port}`);
+});
