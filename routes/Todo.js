424ba164758f527067be5745ae4d5a3203f3d6b9
phuthanh99 <huynhnguyenphuthanh@gmail.com>
2022-03-28 16:19:21 +0700

    add

---
+++ routes/Todo.js
@@ -0,0 +1,23 @@
+﻿const express = require('express');
+const router = express.Router();
+
+const {
+    getAllTodo,
+    createTodo,
+    getTodoId,
+    getTodo,
+    updateTodo,
+    deleteTodo
+} = require('../controllers/Todo');
+
+router.get('/todos/', getAllTodo);
+router.post('/todos/create', createTodo);
+
+router.param('Id', getTodoId);
+
+router.get('/todos/:Id', getTodo);
+
+router.put('/todos/:Id/update', updateTodo);
+router.delete('/todos/:Id/delete', deleteTodo);
+
+module.exports = router;
